1ad6f64ef5ee90470bedeb51708cca9439ed29a7
wagishi <wash16152327@gmail.com>
2026-06-02 22:33:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+import pickle as pk
+from sklearn.feature_extraction.text import TfidfVectorizer
+import streamlit as st
+
+model = pk.load(open('model.pkl','rb'))
+scaler = pk.load(open('scaler.pkl','rb'))
+review = st.text_input('Enter Movie Review')
+
+if st.button('Predict'):
+    review_scale = scaler.transform([review]).toarray()
+    result = model.predict(review_scale)
+    if result[0] == 0:
+        st.write('Negative Review')
+    else:
+        st.write('Positive Review')
